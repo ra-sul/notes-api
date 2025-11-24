141082f4331d7b92a4c4ae14b1e5695508844ec7
Rasul <rasuliskhakov007@gmail.com>
2025-11-24 22:07:24 +0500

    Поменял базу данных на PostgreSQL

--- src/app/database.py
+++ src/app/database.py
@@ -2,9 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from src.app.models.base import Base
 
-engine = create_engine("sqlite:///./data/test.db")
+DATABASE_URL = "postgresql+psycopg2://project_user:qwerty@localhost:5432/myproject"
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 def init_db():
+	from src.app.database import DATABASE_URL
+	print("FACTUAL DATABASE_URL:", DATABASE_URL)
 	Base.metadata.create_all(engine)
