e622947bad42c3a7a770fa83623230a64d3e2160
Rasul <rasuliskhakov007@gmail.com>
2025-11-25 19:50:29 +0500

    Добавлена работа с .env

--- src/app/database.py
+++ src/app/database.py
@@ -1,14 +1,16 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from src.app.models.base import Base
 
-DATABASE_URL = "postgresql+psycopg2://project_user:qwerty@localhost:5432/myproject"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 def init_db():
-	from src.app.database import DATABASE_URL
-	print("FACTUAL DATABASE_URL:", DATABASE_URL)
 	Base.metadata.create_all(engine)
