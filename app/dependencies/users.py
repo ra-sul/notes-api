b04f136ede15c1833ce8d5a584740b0912be74e5
kali <kali@kali.kali>
2026-03-12 17:36:34 +0800

    remove get_current_user from dependecies

--- app/dependencies/users.py
+++ app/dependencies/users.py
@@ -6,19 +6,6 @@ from app.repositories.users import UserRepository
 from app.services.users import UserService
 from app.dependencies.db import get_db
 
-def get_current_user(request: Request, db: Session = Depends(get_db)) -> User:
-    user_id = request.session.get("user_id")
-    if not user_id:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
-    
-    user = db.get(User, user_id)
-
-    if not user:
-        request.session.clear()
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
-    
-    return user
-
 
 def get_user_repo(db: Session = Depends(get_db)) -> UserRepository:
     return UserRepository(db)
